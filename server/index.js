2c54f7b8fb27db9090da28fb5f01a34b12ce75fc
Khalid Farooq <kfcodermdeveloper@gmail.com>
2024-08-12 04:43:27 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://notes-app-frontend-wine.vercel.app",
     credentials: true
 
 }))
